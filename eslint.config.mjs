6fcaebdb96593814d9936a84737d263d6f558a5e
nix <126514998+gnic93@users.noreply.github.com>
2025-05-14 12:48:31 -0300

    Se configuró ESLint con soporte para Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ import eslintPluginPrettier from "eslint-plugin-prettier";
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js, prettier: eslintPluginPrettier }, extends: ["js/recommended"], rules: { "no-console": "warn" } },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js, prettier: eslintPluginPrettier }, extends: ["js/recommended", "plugin:prettier/recommended"], rules: { "no-console": "warn", "prettier/prettier": "error" } },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
 ]);
